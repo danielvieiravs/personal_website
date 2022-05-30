5ff635f98025296d86bf43eee9d2ca89dcf2b405
danielvieiravs <danielvieiravs@live.com.pt>
2022-05-30 16:48:32 +0100

    create structure

---
+++ main/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/projects">Projects</router-link>
+    <router-link to="/posts">Posts</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style lang="scss">
+html {
+  background: white;
+  color: #1c1e21;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+nav {
+  padding: 30px;
+  text-align: right;
+
+  a {
+    font-weight: bold;
+    text-decoration: none;
+    color: #1c1e21;
+    padding: 0px 10px;
+
+    &.router-link-exact-active {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
---
+++ main/src/views/PostsView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="posts">
+    <h1>This is an posts page</h1>
+  </div>
+</template>
